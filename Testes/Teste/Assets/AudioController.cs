d0b89d641e7eeeef4f4b3160a821eba1ba81f1f0
Gabrielle Brandenburg dos Anjos <gabriellebrandenburg@gmail.com>
2020-06-09 01:14:27 -0300

    teste usando audio mixer e mixer groups

---
+++ Testes/Teste/Assets/AudioController.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+// [System.Serializable] 
+public struct Animal {
+	public float highCutoffFreq;
+	public float lowCutoffFreq;
+}
+
+        // for (int i = 0; i < data.Length; i++) {
+        //     data[i] = (float)(rand.NextDouble() * 2.0 - 1.0 + offset);
+        // }
+
+public class AudioController : MonoBehaviour {
+    
+    [Range(-1f, 1f)]
+    float offset;
+    System.Random rand = new System.Random();
+
+    public bool owlMode;
+
+    Animal owl;
+    
+    AudioLowPassFilter lowPassFilter;
+    AudioHighPassFilter highPassFilter;
+
+ 	void Awake() {
+ 		owl.lowCutoffFreq = 800;
+ 		owl.highCutoffFreq = 12000;
+
+        lowPassFilter = GetComponent<AudioLowPassFilter>();
+        highPassFilter = GetComponent<AudioHighPassFilter>();
+        Update();
+    }
+
+    void Update() {
+        lowPassFilter.cutoffFrequency = owlMode ? owl.lowCutoffFreq : 31;
+        highPassFilter.cutoffFrequency = owlMode ? owl.highCutoffFreq : 20000;
+    }
+
+    void OnAudioFilterRead(float[] data, int channels) {
+
+    }
+
+
+
+    
+}
