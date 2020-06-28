1d045d0cf503502f22dbbdfb2abad123f262ce05
Bianca Fragoso <biafrag@tecgraf.puc-rio.br>
2020-06-28 17:43:55 -0300

    Dando zoom e no segundo click entrando na visão e continuando com audio daonde parou

--- JogoAnimals/Assets/Scripts/CameraCloser.cs
+++ JogoAnimals/Assets/Scripts/CameraCloser.cs
@@ -8,18 +8,54 @@ public class CameraCloser : MonoBehaviour
     public GameObject cam1;
     public GameObject cam2;
     public int count = 0;
+    public string Status;
+    public Transform currentView;
+    private bool doIt = false;
+    public AudioSource audioMain;
+    public AudioSource audioOwl;
+
+
     public void zoomIn()
     {
-        if(count <= 0)
+
+        transform.position = Vector3.Lerp(transform.position, currentView.position, Time.deltaTime * 2);
+
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        if(doIt)
         {
-            cam.fieldOfView = Mathf.Lerp(cam.fieldOfView, -1500, Time.deltaTime * 2);
-            count++;
+            zoomIn();
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(count == 1)
+        {
+            if (doIt == false)
+            {
+                doIt = true;
+            }
         }
         else
         {
-            cam1.SetActive(false);
-            cam2.SetActive(true);
+            if(doIt)
+            {
+                cam1.SetActive(false);
+                float time = audioMain.time;
+                audioOwl.SetScheduledStartTime(time);
+                cam2.SetActive(true);
 
+                print(Status);
+            }
         }
+    }
+    public void Activate()
+    {
+        count++;
     }
 }
