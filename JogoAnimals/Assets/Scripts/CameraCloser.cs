9456a2e3353d9dbd2529887c853f47f8960462c0
Bianca Fragoso <biafrag@tecgraf.puc-rio.br>
2020-06-28 01:00:45 -0300

    Dinâmica de camera começando

---
+++ JogoAnimals/Assets/Scripts/CameraCloser.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraCloser : MonoBehaviour
+{
+    public Camera cam;
+    public GameObject cam1;
+    public GameObject cam2;
+    public int count = 0;
+    public void zoomIn()
+    {
+        if(count <= 0)
+        {
+            cam.fieldOfView = Mathf.Lerp(cam.fieldOfView, -1500, Time.deltaTime * 2);
+            count++;
+        }
+        else
+        {
+            cam1.SetActive(false);
+            cam2.SetActive(true);
+
+        }
+    }
+}
