a292be0c3856b92ed9586cce6ae21e2ce3d7a6e9
Gabrielle Brandenburg dos Anjos <gabriellebrandenburg@gmail.com>
2020-07-06 04:22:58 -0300

    timer trigando o closing da camera

--- JogoAnimals/Assets/Scripts/GazeVR.cs
+++ JogoAnimals/Assets/Scripts/GazeVR.cs
@@ -1,4 +1,4 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
@@ -11,6 +11,10 @@ public class GazeVR : MonoBehaviour
 	bool gvrStatus;
 	float gvrTimer;
 
+	bool inSpot = false;
+
+	public int distanceOfRay = 10;
+	private RaycastHit _hit;
 
     // Start is called before the first frame update
     void Start()
@@ -25,6 +29,25 @@ public class GazeVR : MonoBehaviour
         {
         	gvrTimer += Time.deltaTime;
         	imgGaze.fillAmount = gvrTimer / totalTime;
+        }
+
+        Ray ray = Camera.main.ViewportPointToRay(new Vector3(0.5f, 0.5f, 0f));
+
+        if(Physics.Raycast(ray, out _hit, distanceOfRay))
+        {
+
+        	if(imgGaze.fillAmount == 1 && _hit.transform.CompareTag("Owl"))
+        	{
+        		print("hit owl!");
+        		if (!inSpot)
+        		{
+        			print("zoom on");
+        			imgGaze.fillAmount = 0;
+        			inSpot = true; //triggar 1 vez só (se nao crasha)
+        			_hit.transform.gameObject.GetComponent<ZoomIn>().zoomIn();
+
+        		}
+        	}
         }
     }
 
---
+++ JogoAnimals/Assets/Scripts/ZoomIn.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ZoomIn : MonoBehaviour
+{
+	public GameObject playerCamera;
+	public Transform animalSpot;
+
+	bool zoom = false;
+
+	public void zoomIn()
+    {
+    	zoom = true;
+    }
+
+    void LateUpdate()
+    {
+        if(zoom)
+        {
+        	print("zooming");
+            playerCamera.transform.position = Vector3.MoveTowards(playerCamera.transform.position, animalSpot.position, Time.deltaTime * 2);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(playerCamera.transform.position == animalSpot.position)
+        {
+        	zoom = false;
+        	print("zoom off");
+        }
+    }
+
+    public void Activate()
+    {
+        // count++;
+        // zoom = true;
+    }
+}
