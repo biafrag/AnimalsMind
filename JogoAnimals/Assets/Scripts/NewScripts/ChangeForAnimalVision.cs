dff5ddf8947dd04e86e082355925d000ff42eb7f
Bianca Fragoso <biafrag@tecgraf.puc-rio.br>
2020-07-10 21:34:36 -0300

    Com cobra mas visão não 100% ainda

--- JogoAnimals/Assets/Scripts/NewScripts/ChangeForAnimalVision.cs
+++ JogoAnimals/Assets/Scripts/NewScripts/ChangeForAnimalVision.cs
@@ -8,6 +8,7 @@ public class ChangeForAnimalVision : MonoBehaviour
     public GameObject[] Owl;
     public GameObject[] Dog;
     public GameObject[] Cat;
+    public GameObject[] Snake;
     public string currentAnimal = "Bolotas";
     public string CurrentAnimal;
     enum Part
@@ -47,6 +48,16 @@ public class ChangeForAnimalVision : MonoBehaviour
             //mainAudio.outputAudioMixerGroup = B.outputAudioMixerGroup;
             currentAnimal = animal;
         }
+        else if (string.Compare(animal, "Snake") == 0)
+        {
+
+            Snake[(int)Part.PostProcessing].layer = LayerMask.NameToLayer("PostProcessing");
+            transform.position = Snake[(int)Part.Animal].transform.position;
+            snakeMode(true);
+            //AudioSource mainAudio = GameObject.FindObjectOfType<AudioSource>();
+            //mainAudio.outputAudioMixerGroup = B.outputAudioMixerGroup;
+            currentAnimal = animal;
+        }
         currentAnimal = animal;
         GetComponent<GazeInside>().imgGaze.fillAmount = 0;
 
@@ -68,6 +79,41 @@ public class ChangeForAnimalVision : MonoBehaviour
         {
             Cat[(int)Part.PostProcessing].layer = LayerMask.NameToLayer("Default");
         }
+        else if (string.Compare(currentAnimal, "Snake") == 0)
+        {
+            snakeMode(false);
+            Snake[(int)Part.PostProcessing].layer = LayerMask.NameToLayer("Default");
+        }
         change(animalGo);
+    }
+
+    void snakeMode(bool isSnake)
+    {
+        S = "entrei";
+        print(S);
+        GameObject[] animals;
+        animals = GameObject.FindGameObjectsWithTag("Animals");
+
+        if(isSnake)
+        {
+            foreach (GameObject a in animals)
+            {
+                a.GetComponent<Renderer>().material.EnableKeyword("_EMISSION");
+                S = "entrei";
+                print(S);
+            }
+        }
+        else
+        {
+            foreach (GameObject a in animals)
+            {
+                a.GetComponent<Renderer>().material.DisableKeyword("_EMISSION");
+                S = "entrei";
+                print(S);
+            }
+
+            S = "entrei";
+            print(S);
+        }
     }
 }
